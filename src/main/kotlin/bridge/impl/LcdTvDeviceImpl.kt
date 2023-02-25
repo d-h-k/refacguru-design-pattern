79dad6be4febdd835918144a335d9e91cc9fdf3f
dongdongKing <ultraraptor@naver.com>
2023-02-25 10:04:29 +0900

    2월 25일 토요일

---
+++ src/main/kotlin/bridge/impl/LcdTvDeviceImpl.kt
@@ -0,0 +1,49 @@
+package bridge.impl
+
+import bridge.abs.TvDevice
+
+class LcdTvDeviceImpl:TvDevice {
+    override fun isEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun enabled() {
+        TODO("Not yet implemented")
+    }
+
+    override fun disabled() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getVolume(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChannel(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getVolume(percent: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setChannel(channel: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun analogBroadcastCompatibility() {
+        TODO("Not yet implemented")
+    }
+
+    override fun digitalBroadcastCompatibility() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getResolution() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDisplayRatio() {
+        TODO("Not yet implemented")
+    }
+}
